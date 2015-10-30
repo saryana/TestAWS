1eed9b4598f07879c8cc57b764e784e292d2a084
Sean Aryana <saryana0@cs.washington.edu>
2015-10-29 17:00:55 -0700

    stuff

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Created by saryana on 10/29/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime time = new LocalTime();
+        System.out.println("Current time is: " + time);
+    }
+}
